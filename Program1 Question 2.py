9aafd853dd3ced98cca2a85a391f788ac83b8204
doquan01 <55120808+doquan01@users.noreply.github.com>
2020-09-16 19:32:03 -0700

    Created mothod to convert binary to decimal

--- Program1 Question 2.py
+++ Program1 Question 2.py
@@ -1,3 +1,15 @@
+def convert(x):
+    x = int(x)
+    decimal = 0
+    power = 1
+    while (x>0):
+        last = x % 10
+        x = int(x / 10)
+        decimal += (last * power)
+        power = power * 2
+    return decimal
+        
 binary = input("Enter a binary string\n") #take in user input
 
+print(convert (binary)) #convert binary string into decimal using method
 print(int (binary,2)) #convert binary string into base 10 integer with inbuilt int
